f653492914423ed12627523756d7b28640e7db9a
vinicius marques <viniciusmc486@gmail.com>
2024-05-08 12:32:14 -0300

    add aulas 37 a 48

---
+++ modulo3_logica_de_programacao/aula41_exercicio_com_logica/aula41.js
@@ -0,0 +1,10 @@
+// Escreva uma função que recebe 2 números e retorne o maior deles
+
+function maiorNumero(x, y) {
+    return x > y ? x : y;
+}
+
+console.log(maiorNumero(56, 300));
+
+const max2 = (x, y) => x > y ? x : y;
+console.log(max2(10, 5));
