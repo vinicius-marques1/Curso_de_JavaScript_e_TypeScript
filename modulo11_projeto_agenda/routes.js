ac6f2dd39d63ee1fc7b45e17944314089b47bd46
vinicius marques <viniciusmc486@gmail.com>
2026-02-18 22:05:12 -0300

    add new class about a project for a scheduling website

--- modulo11_projeto_agenda/routes.js
+++ modulo11_projeto_agenda/routes.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const route = express.Router();
 const homeController = require('./src/controllers/homeController');
-const contatoController = require('./src/controllers/contatoController');
+const loginController = require('./src/controllers/loginController');
 
 // Rotas da home
-route.get('/', homeController.paginaInicial);
-route.post('/', homeController.trataPost);
-// Rotas de contato
-route.get('/contato', contatoController.paginaInicial);
+route.get('/', homeController.index);
 
+// Rotas de login
+route.get('/login/index', loginController.index);
+route.post('/login/register', loginController.register);
 
 module.exports = route;
