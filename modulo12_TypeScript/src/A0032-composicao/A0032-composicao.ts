81e8fccced5b946b461a64175e0524864bbc33f9
vinicius marques <viniciusmc486@gmail.com>
2026-07-26 22:02:25 -0300

    feat: add video player functionality with HTML, CSS, and TypeScript integration

---
+++ modulo12_TypeScript/src/A0032-composicao/A0032-composicao.ts
@@ -0,0 +1,44 @@
+export class Carro {
+    private readonly motor = new Motor();
+
+    ligar(): void {
+        this.motor.ligar();
+    }
+
+    acelerar(): void {
+        this.motor.acelerar();
+    }
+
+    parar(): void {
+        this.motor.parar();
+    }
+
+    desligar(): void {
+        this.motor.desligar();
+    }
+}
+
+export class Motor {
+    ligar(): void {
+        console.log('Motor está ligado...');
+    }
+
+    acelerar(): void {
+        console.log('Motor está acelerando...');
+    }
+
+    parar(): void {
+        console.log('Motor está parado...');
+    }
+
+    desligar(): void {
+        console.log('Motor está desligado...');
+    }
+}
+
+const carro = new Carro();
+
+carro.ligar();
+carro.acelerar();
+carro.parar();
+carro.desligar();
